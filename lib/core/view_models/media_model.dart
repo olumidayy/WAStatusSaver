6c36c3817a8dd36e26b2dde957ea396f085220a0
olumidayy <niolumi4eva@gmail.com>
2021-08-04 09:31:23 +0100

    tried to update

--- lib/core/view_models/media_model.dart
+++ lib/core/view_models/media_model.dart
@@ -4,51 +4,50 @@ import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
 import 'package:status_saver/core/services/services.dart';
 import 'package:status_saver/ui/common/delete_dialog.dart';
-import 'package:video_thumbnail/video_thumbnail.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:thumbnails/thumbnails.dart';
 
 class MediaModel extends ChangeNotifier {
-  static const statusPath = '/storage/emulated/0/WhatsApp/Media/.Statuses';
-  static const gbStatusPath = '/storage/emulated/0/GBWhatsApp/Media/.Statuses';
-  static const businessStatusPath =
-      '/storage/emulated/0/WhatsApp Business/Media/.Statuses';
+  static const statusFolder = 'WhatsApp';
+  static const gbStatusFolder = 'GBWhatsApp';
+  static const businessStatusFolder = 'WhatsApp Business';
+  //'/storage/emulated/0/$statusFolder/Media/.Statuses'
+  String _currentFolder = statusFolder;
 
-  String _currentPath = statusPath;
-
-  String get currentPath => _currentPath;
+  String get currentPath => _currentFolder;
 
   static const savePath = '/storage/emulated/0/WAStatusSaver';
 
-  List<File> get photos => _fetchMedia(_currentPath);
+  List<File> get photos => _fetchMedia();
 
-  List<File> get videos => _fetchMedia(_currentPath, images: false);
+  List<File> get videos => _fetchMedia(images: false);
 
-  List<File> get savedPhotos => _fetchMedia(savePath);
+  List<File> get savedPhotos => _fetchMedia(path: savePath);
 
-  List<File> get savedVideos => _fetchMedia(savePath, images: false);
+  List<File> get savedVideos => _fetchMedia(path: savePath, images: false);
 
-  setState(){
+  setState() {
     notifyListeners();
   }
 
   void setPath(String arg) {
     switch (arg) {
       case 'gb':
-        _currentPath = gbStatusPath;
+        _currentFolder = gbStatusFolder;
         break;
 
       case 'business':
-        _currentPath = businessStatusPath;
+        _currentFolder = businessStatusFolder;
         break;
 
       case 'wa':
-        _currentPath = statusPath;
+        _currentFolder = statusFolder;
         break;
       default:
         throw Error();
     }
     notifyListeners();
-    print(_currentPath);
+    print(_currentFolder);
   }
 
   Future<void> saveFile(File file, {bool isimage = true}) async {
@@ -69,32 +68,49 @@ class MediaModel extends ChangeNotifier {
     }
   }
 
-  List<File> _fetchMedia(String path, {bool images = true}) {
-    List<File> photos;
-    try {
-      photos = Directory(path)
-          .listSync()
-          .where((item) => item.path.endsWith(images ? 'jpg' : 'mp4'))
-          .map((e) => (e as File))
-          .toList();
-      photos
-          .sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
-    } catch (e) {
-      print(e);
-      photos = [];
-    }
-
-    return photos;
+  readFromPath(String path, {bool images = true}) {
+    return Directory(path)
+        .listSync()
+        .where((item) => item.path.endsWith(images ? 'jpg' : 'mp4'))
+        .map((e) => (e as File))
+        .toList();
   }
 
+  List<File> _fetchMedia({String path, bool images = true}) {
+    List<String> paths = [
+      '/storage/emulated/0/Android/media/com.whatsapp/$statusFolder/Media/.Statuses',
+      '/storage/emulated/0/$statusFolder/Media/.Statuses'
+    ];
+    List<File> media = [];
+    if (path != null) {
+      media = readFromPath(path);
+      print(readFromPath(path, images: false));
+    } else {
+      for (var p in paths) {
+        try {
+          media.addAll(readFromPath(p));
+        } catch (e) {}
+      }
+    }
+    if (media.isNotEmpty)
+      media
+          .sort((a, b) => b.lastModifiedSync().compareTo(a.lastModifiedSync()));
+    print(media);
+    // var vids = readFromPath('/storage/emulated/0/Android/media/com.whatsapp/$statusFolder/Media/.Statuses', images: false);
+      // print(await fetchVidThumbnail(vids[0].path));
+    media.removeLast();
+    return media;
+  }
 
   Future<Uint8List> fetchVidThumbnail(String path) async {
-    final thumbnail = await VideoThumbnail.thumbnailData(
-      video: path,
-      imageFormat: ImageFormat.JPEG,
-      maxWidth: 200,
-      quality: 100,
-    );
+    var thumbnail;
+    try {
+        thumbnail = await Thumbnails.getThumbnail(
+        thumbnailFolder: '/storage/emulated/0/Videos/Thumbnails',
+        videoFile: path,
+        imageType: ThumbFormat.PNG,
+        quality: 30);
+    } catch (e) { print(e); }
     return thumbnail;
   }
 
